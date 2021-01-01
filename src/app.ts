d6942feb0b9c894accd242e68405fdd1fef82e96
VladTzaru <bvanja.87@gmail.com>
2021-01-01 19:19:18 +0100

    Added dom pointers

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,47 @@ class ProjectInput {
   templateEl: HTMLTemplateElement;
   renderEl: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
 
   constructor() {
     this.templateEl = document.getElementById(
       'project-input'
-    )! as HTMLTemplateElement;
-    this.renderEl = document.getElementById('app')! as HTMLDivElement;
+    ) as HTMLTemplateElement;
+    this.renderEl = document.getElementById('app') as HTMLDivElement;
 
     const importedNode = document.importNode(this.templateEl.content, true);
-    this.element = importedNode.firstElementChild! as HTMLFormElement;
-    console.log(this.element);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputEl = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+
+    this.descriptionInputEl = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+
+    this.peopleInputEl = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
     this.append();
   }
 
   private append() {
     this.renderEl.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputEl.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 }
 
